f30b9a2583fa9bc07c6581e9798710ca75a85b81
thruc <horiuchi.github@gmail.com>
2022-05-09 21:31:47 +0900

    add ownership

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod vars;
+mod ownership;
 mod stack_heap;
 fn main() {
     println!("Hello, world!");
     vars::run();
+    ownership::run();
     stack_heap::run();
     vars::sub_a::func_a();
     vars::sub_b::func_b();
