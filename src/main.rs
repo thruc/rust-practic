6bf87293f6791c7e8df61e7e99fb07e558c9a3bd
thruc <horiuchi.github@gmail.com>
2022-05-09 22:56:30 +0900

    comp

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-mod vars;
+mod enums;
+mod error_handling;
+mod generics;
+mod lifetime;
 mod ownership;
 mod stack_heap;
+mod structs;
+mod traits;
+mod unit_test;
+mod vars;
+
 fn main() {
     println!("Hello, world!");
     vars::run();
-    ownership::run();
     stack_heap::run();
+    ownership::run();
+    generics::run();
+    lifetime::run();
+    structs::run();
+    enums::run();
+    traits::run();
+    error_handling::run();
     vars::sub_a::func_a();
     vars::sub_b::func_b();
 }
